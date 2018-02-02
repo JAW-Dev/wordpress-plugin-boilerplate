efe8c56e94f55c2f3564f18db2bb7e6755e206e5
Jason Witt <jawittdesigns@gmail.com>
2018-02-02 12:11:10 -0500

    Updated autoload to do recursive search

--- includes/autoload.php
+++ includes/autoload.php
@@ -2,9 +2,9 @@
 /**
  * Autoloader
  *
- * @package    {{plugin-package}}
- * @subpackage {{plugin-package}}/Includes
- * @author     Plugin_Author <{{plugin-author-email}}>
+ * @package    Plugin_Package
+ * @subpackage Plugin_Package/Includes
+ * @author     Plugin_Author <Plugin_Author_Email>
  * @copyright  Copyright (c) 2018, Plugin_Author
  * @license    GNU General Public License v2 or later
  * @version    1.0.0
@@ -15,9 +15,10 @@ namespace Plugin_Package\Includes;
 /**
  * Autoloader
  *
- * @param string $class Name of the class being requested.
+ * @author Plugin_Author
+ * @since  1.0.0
  *
- * @since 1.0.0
+ * @param string $class Name of the class being requested.
  *
  * @return void
  */
@@ -28,17 +29,30 @@ function _autoload_classes( $class ) {
 
 	// Full path to the classes directory.
 	$path  = trailingslashit( plugin_dir_path( __FILE__ ) ) . trailingslashit( 'classes' );
+	$files = new \RecursiveDirectoryIterator( $path );
 
-	// Constructed file with full path to autoload.
-	$file  = $path . 'class-' . strtolower( str_replace( '_', '-', end( $class ) ) ) . '.php';
+	// Loop through the files.
+	foreach ( new \RecursiveIteratorIterator( $files ) as $file ) {
 
-	// Add classes to be excluded from autoloading. example: array( $path . 'class-name.php' );.
-	$excludes  = array();
+		$filename = $file->getFilename();
+		$filepath = $file->getPath();
 
-	// Require file if the file exists and is not in the excludes list.
-	if ( file_exists( $file ) && ! in_array( $file, $excludes, true ) ) {
+		// Exclude dot files.
+		if ( '.' === substr( $filename, 0, 1 ) ) {
+			continue;
+		}
 
-		require_once( $file );
+		// Constructed file with full path to autoload.
+		$file_fullpath = trailingslashit( $filepath ) . 'class-' . strtolower( str_replace( '_', '-', end( $class ) ) ) . '.php';
+
+		// Add classes to be excluded from autoloading. example: array( $path . 'class-name.php' );.
+		$excludes = array();
+
+		// Require file if the file exists and is not in the excludes list.
+		if ( file_exists( $file_fullpath ) && ! in_array( $file_fullpath, $excludes, true ) ) {
+
+			require_once( $file_fullpath );
+		}
 	}
 }
 spl_autoload_register( __NAMESPACE__ . '\\_autoload_classes' );
